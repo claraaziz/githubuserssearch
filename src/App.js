a66e30099d544ba67ff8e1f2e8d92d027483bceb
claraaziz <cocca.az@gmail.com>
2023-06-01 00:56:17 +0300

    add: styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
@@ -26,20 +26,26 @@ const getUsers = async (query) => {
     event.preventDefault();
     const results = await getUsers(query);
     setResults(results.items);
+    console.log(results)
   }
 
   return (
-    <div className="">
-      <h1>Search for github users</h1>
+    <div className="justify-center items-center flex flex-col min-h-screen bg-slate-950">
+      <h1 className="text-slate-100 mt-20 text-4xl font-bold font-mono mb-5">Search for github users</h1>
       <form onSubmit={onSearchSubmit}>
-        <input type='text' value={query} onChange={updateQuery}/>
-        <input type='submit'/>
+        <input type='text' value={query} placeholder="Write a username.." onChange={updateQuery} className="transition ease-in-out h-12 w-80 rounded-md pl-5 text-lg font-mono bg-slate-300 focus:bg-slate-200 outline-0"/>
+        <input type='submit' className='transition ease-in-out bg-slate-600 text-slate-100 ml-5 h-12 w-24 text-lg font-semibold rounded-md font-mono cursor-pointer hover:bg-slate-500 active:bg-slate-400'/>
       </form>
-      <div>
+      <div className='mt-5 mb-20'>
         {
-          results ? results.map(user=><div>
-            <img src={user.avatar_url}/>
-            <a href={user.html_url} target="_blank">{user.login}</a>
+          results ? results.map(user=><div className='bg-slate-800 rounded-lg flex flex-row mb-3 pt-3 pb-3 pl-5 pr-5'>
+            <img src={user.avatar_url} alt="userimg" className='rounded-full w-2/12 mr-5'/>
+            <div className='flex flex-col justify-center'>
+              <p className='text-slate-300 font-mono font-bold text-2xl'>{user.login}</p>
+              <div>
+                <button className='transition ease-in-out text-slate-900 font-mono font-semibold bg-cyan-500 pb-1 pt-1 pl-2 pr-2 rounded-md mt-2 hover:bg-cyan-400 active:bg-cyan-300 '><a href={user.html_url} target="_blank" rel="noreferrer">Visit Profile</a></button>
+              </div>
+            </div>
           </div>) : " "
         }
       </div>
