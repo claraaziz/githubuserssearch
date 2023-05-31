bba2f1e3f9e191f1b513e9e997bc220d56e161b0
claraaziz <cocca.az@gmail.com>
2023-06-01 01:09:29 +0300

    add: morph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { useMorph } from 'react-morph';
 import axios from 'axios';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
   const[results, setResults] = useState([]);
   const api = "https://api.github.com";
   const [users, setUsers] = useState([]);
+  const morph = useMorph();
 
 const getUsers = async (query) => {
   try{
@@ -30,7 +32,7 @@ const getUsers = async (query) => {
   }
 
   return (
-    <div className="justify-center items-center flex flex-col min-h-screen bg-slate-950">
+    <div {...morph} className="justify-center items-center flex flex-col min-h-screen bg-slate-950">
       <h1 className="text-slate-100 mt-20 text-4xl font-bold font-mono mb-5">Search for github users</h1>
       <form onSubmit={onSearchSubmit}>
         <input type='text' value={query} placeholder="Write a username.." onChange={updateQuery} className="transition ease-in-out h-12 w-80 rounded-md pl-5 text-lg font-mono bg-slate-300 focus:bg-slate-200 outline-0"/>
@@ -38,7 +40,7 @@ const getUsers = async (query) => {
       </form>
       <div className='mt-5 mb-20'>
         {
-          results ? results.map(user=><div className='bg-slate-800 rounded-lg flex flex-row mb-3 pt-3 pb-3 pl-5 pr-5'>
+          results ? results.map(user=><div {...morph} className='bg-slate-800 rounded-lg flex flex-row mb-3 pt-3 pb-3 pl-5 pr-5'>
             <img src={user.avatar_url} alt="userimg" className='rounded-full w-2/12 mr-5'/>
             <div className='flex flex-col justify-center'>
               <p className='text-slate-300 font-mono font-bold text-2xl'>{user.login}</p>
